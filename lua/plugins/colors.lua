f143f734e71d260ad0139d279d4dd6c747c28418
mhsn <mail@mhsn.net>
2025-02-25 12:01:35 +0000

    change color theme from gruvbox-material to everforest

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,10 +1,12 @@
 return {
 	{
-		"sainnhe/gruvbox-material",
+		"sainnhe/everforest",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd([[colorscheme gruvbox-material]])
+			vim.g.everforest_background = "hard"
+			vim.g.everforest_disable_italic_comment = true
+			vim.cmd.colorscheme("everforest")
 		end,
 	},
 }
