ddb678e56144b14c02bc1ef14120720079e9582a
mhsn <mail@mhsn.net>
2025-02-25 13:03:33 +0000

    add vim, vimdoc to nvim-treesitter ensure_installed

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -4,7 +4,7 @@ return {
 		build = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "lua", "python", "rust" },
+				ensure_installed = { "vim", "vimdoc", "lua", "python", "rust" },
 				sync_install = false,
 				highlight = { enable = true },
 				indent = { enable = true },
