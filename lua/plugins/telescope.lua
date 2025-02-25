2d0d6f6d944420e13a54b0cec602d384abf4cf39
mhsn <mail@mhsn.net>
2025-02-25 13:15:14 +0000

    add a few telescope keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,8 +2,11 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		keys = {
-			{ "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
 			{ "<C-b>", "<cmd>Telescope keymaps<cr>", desc = "Telescope search keymaps" },
+			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
+			{ "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Telescope git files" },
+			{ "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Telescope git commits" },
+			{ "<leader>gg", "<cmd>Telescope git_status<cr>", desc = "Telescope git status" },
 		},
 	},
 }
