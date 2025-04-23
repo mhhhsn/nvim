5a4a99f0eba4c95578fc181da168fe62dba0c3a8
mhsn <mail@mhsn.net>
2025-04-23 10:39:28 +0000

    call require() for mini plugins

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,5 +1,20 @@
 return {
-	{ "echasnovski/mini.align" },
-	{ "echasnovski/mini.pairs" },
-	{ "echasnovski/mini.surround" },
+	{
+		"echasnovski/mini.align",
+		config = function()
+			require("mini.align").setup()
+		end
+	},
+	{
+		"echasnovski/mini.pairs",
+		config = function()
+			require("mini.pairs").setup()
+		end
+	},
+	{
+		"echasnovski/mini.surround",
+		config = function()
+			require("mini.surround").setup()
+		end
+	},
 }
