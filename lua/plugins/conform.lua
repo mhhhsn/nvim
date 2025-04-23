3e5af33ee669f48582592ed5ae221a6f6ece239c
mhsn <mail@mhsn.net>
2025-04-23 10:08:46 +0000

    add shfmt formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,6 +5,7 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
+				sh = { "shfmt" }
 			},
 			format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
 		},
