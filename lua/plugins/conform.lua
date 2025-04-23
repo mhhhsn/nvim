92e2e0766da3f55f716fbbbe4a89a2ea5e745238
mhsn <mail@mhsn.net>
2025-04-23 10:19:57 +0000

    add mdformat formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,6 +4,7 @@ return {
 		opts = {
 			formatters_by_ft = {
 				lua = { "stylua" },
+				markdown = { "mdformat" },
 				python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
 				sh = { "shfmt" }
 			},
