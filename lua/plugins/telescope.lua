d8fe49d5fd29d338ac82e610a37a61ed7e8a5b6a
mhsn <mail@mhsn.net>
2025-04-30 14:36:10 +0100

    change telescope keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,11 +2,13 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		keys = {
-			{ "<C-b>", "<cmd>Telescope keymaps<cr>", desc = "Telescope search keymaps" },
-			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
-			{ "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Telescope git files" },
+			{ "<C-b>",      "<cmd>Telescope keymaps<cr>",     desc = "Telescope search keymaps" },
+			{ "<leader>FF", "<cmd>Telescope find_files<cr>",  desc = "Telescope find files" },
+			{ "<leader>ff", "<cmd>Telescope git_files<cr>",   desc = "Telescope git files" },
+			{ "<leader>ff", "<cmd>Telescope git_files<cr>",   desc = "Telescope git files" },
 			{ "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Telescope git commits" },
-			{ "<leader>gg", "<cmd>Telescope git_status<cr>", desc = "Telescope git status" },
+			{ "<leader>gg", "<cmd>Telescope live_grep<cr>",   desc = "Telescope live grep" },
+			{ "<leader>gs", "<cmd>Telescope git_status<cr>",  desc = "Telescope git status" },
 		},
 	},
 }
